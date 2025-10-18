a1089bd51bc739b17ad7ab5d1745e42220d9e0a1
sk0rpzmey <sk0rpzmey@ya.ru>
2025-10-18 15:52:57 +0300

    fix github checkStyle

--- src/ru/practicum/manager/ManagerSaveException.java
+++ src/ru/practicum/manager/ManagerSaveException.java
@@ -1,15 +1,15 @@
 package ru.practicum.manager;
 
-public class ManagerSaveException extends RuntimeException{
-    public ManagerSaveException () {
+public class ManagerSaveException extends RuntimeException {
+    public ManagerSaveException() {
         super();
     }
 
-    public ManagerSaveException (String message) {
+    public ManagerSaveException(String message) {
         super(message);
     }
 
-    public ManagerSaveException (String message, Throwable e) {
+    public ManagerSaveException(String message, Throwable e) {
         super(message, e);
     }
 }
